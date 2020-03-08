6af539dcae3f23d18ff8306ea58e808bbbd108aa
Abhinav Dhiman <abhinavg916@gmail.com>
2020-03-08 18:49:17 +0530

    Fixed Authentication java class on authentication failure of the user

--- quora-service/src/main/java/com/upgrad/quora/service/exception/AuthenticationFailedException.java
+++ quora-service/src/main/java/com/upgrad/quora/service/exception/AuthenticationFailedException.java
@@ -3,10 +3,6 @@ package com.upgrad.quora.service.exception;
 import java.io.PrintStream;
 import java.io.PrintWriter;
 
-/**
- * AuthenticationFailedException is thrown in case of authentication failure.
- *
- */
 public class AuthenticationFailedException extends Exception {
     private final String code;
     private final String errorMessage;
