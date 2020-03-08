9a45715fa0485dcc56d6de0e71c5e642c2fdaa30
Abhinav Dhiman <abhinavg916@gmail.com>
2020-03-08 18:54:20 +0530

    Fixed main java class for Quora backend application

--- quora-api/src/main/java/com/upgrad/quora/api/QuoraApiApplication.java
+++ quora-api/src/main/java/com/upgrad/quora/api/QuoraApiApplication.java
@@ -5,10 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-/**
- * A Configuration class that can declare one or more @Bean methods and trigger auto-configuration and component scanning.
- * This class launches a Spring Application from Java main method.
- */
 @SpringBootApplication
 @Import(ServiceConfiguration.class)
 public class QuoraApiApplication {
