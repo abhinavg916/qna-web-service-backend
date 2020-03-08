a82e387ffb4b30e0baced8f3871f77f91717a78f
Abhinav Dhiman <abhinavg916@gmail.com>
2020-03-08 18:36:27 +0530

    Add Java Classes for Exception Handling in quora-service module

---
+++ quora-service/src/main/java/com/upgrad/quora/service/exception/signInException.java
@@ -0,0 +1,40 @@
+package com.upgrad.quora.service.exception;
+
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+public class signInException extends Exception {
+
+    private final String code;
+    private final String errorMessage;
+
+    public signInException(final String code, final String errorMessage) {
+        this.code = code;
+        this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public void printStackTrace() {
+        super.printStackTrace();
+    }
+
+    @Override
+    public void printStackTrace(PrintStream s) {
+        super.printStackTrace(s);
+    }
+
+    @Override
+    public void printStackTrace(PrintWriter s) {
+        super.printStackTrace(s);
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+}
+
